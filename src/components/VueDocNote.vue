4d558017fbd487ed7b218dc9b34b6a511d68a7fa
Siva <sivalayavinod25@gmail.com>
2025-06-11 23:19:20 +0530

    Notes of Vue.js

---
+++ src/components/VueDocNote.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <h1>Creating an Application</h1>
+    <div>
+      <p>Create a new application instance with the createApp() function.</p>
+      <p>
+        <li>The passing object in createApp() function is a Root-Component</li>
+        <li>import root component and imple</li>
+        <li>An application instance won't be run anything until it's Mound() method is called.</li>
+        <li>use multiple components in the root component for a better understanding.</li>
+      </p>
+    </div>
+    <div>
+      <p>Template syntax</p>
+      <p>
+        <li>Text Interpolation.</li>
+        <li>Directives and Mustaches.</li>
+        <li>v-bind for html attributes.</li>
+        <li>Boolean Attributes</li>
+      </p>
+    </div>
+    <div>
+      <p>Reactivity Fundamentals</p>
+      <p>
+        <li>To Declare reactive state we use ref() Function.</li>
+        <li>ref() value is accessible by using the .value</li>
+        <li>In template the ref() value will get automatically Uncovered.</li>
+        <li>reactive() API</li>
+      </p>
+    </div>
+    <div>
+      <p>Computed Properties</p>
+      <p>
+        <li>
+          To make easy to make any other functions with the data inside a reactive() functions, we
+          can use the computed to make it clean the Template.
+        </li>
+        <li></li>
+      </p>
+    </div>
+    <div>
+      <p>Class and Style Bindings</p>
+      <p>
+        <li>we can have the class names with objects.</li>
+      </p>
+    </div>
+    <div>
+      <p>Components Basics</p>
+      <p>
+        <li>Can import components and use it</li>
+        <li>We can define props.</li>
+        <li>In component we can define the props for it.</li>
+        <li>
+          so when we call the component we can give the props and access it in the parent component
+          too.
+        </li>
+        <li>Listening to events.</li>
+        <li>Use Custom Slot.</li>
+        <li>custom slot to do an alert box</li>
+        <li>Use the '<slot />'</li>
+      </p>
+    </div>
+    <div>
+      <p>Lifecylcle Hooks</p>
+      <p>
+        <li>Registering Lifecylcle hooks</li>
+        <li>Hooks for eg: onMounted , onUpdated, and onUnMounted.</li>
+        <li>
+          mounted: Called after the component is mounted to the DOM. DOM elements are now
+          accessible.
+        </li>
+        <li>unMounted: Called after the component is unmounted from the DOM.</li>
+        <li>Lifecylcle Diagram</li>
+      </p>
+    </div>
+    <div>
+      <p>TypeScript with Composition API</p>
+      <p>Overview ofTypeScript.</p>
+      <p>
+        <li>Typing Component Props</li>
+        <li>runtime declaration - eg: definePorps()</li>
+        <li>definePorps with generic type argument - Type-based declaration.</li>
+        <li></li>
+      </p>
+    </div>
+  </div>
+  <div>-------------------</div>
+  <div>
+    <div>
+      <p>TypeScript with Composition API</p>
+      <p>Overview ofTypeScript.</p>
+      <p>
+        <li>Typing Component Props</li>
+        <li>runtime declaration - eg: definePorps()</li>
+        <li>definePorps with generic type argument - Type-based declaration.</li>
+        <li></li>
+      </p>
+    </div>
+    <div>
+      <p>TypeScript</p>
+      <p>
+        <li>The ts promotes the Static Typing.</li>
+        <li>Error handling.</li>
+        <li>Early error detection handling.</li>
+        <li>Static Typing</li>
+        <li>jkytrfdui</li>
+        <li>uityrerfs</li>
+        <li>iuytrefd</li>
+        <li>jhkuyti</li>
+        <li>ytrefdrtuiut</li>
+        <li>uytrefdyi</li>
+        <li>ytresdcxertyui</li>
+        <li>ytrefdiu</li>
+        <li>uytrdfu</li>
+        <li>uytrewsduijkn</li>
+        <li>treyyiujk</li>
+        <li>esfrtttye</li>
+        <li>iuytrfkj</li>
+        <li>uytredf</li>
+        <li>uytrdsukj</li>
+        <li>ytredfuikj</li>
+        <li>uytredfik</li>
+        <li>ewqsdaxczvcfdrt</li>
+        <li>ioupokljmn</li>
+        <li>qewpoilkjdsacxzm</li>
+        <li>rdfriuy</li>
+        <li>redfyuiujkcv</li>
+        <li>lkjhfdsacxweqpoi</li>
+        <li>rtdgfuyncv</li>
+        <li>sfdxvijkntrdf</li>
+        <li>dfcvtyukj</li>
+        <li>wesdfcvertyupoq</li>
+        <li>rdfvikjdfvc</li>
+        <li>ewdsqazcxsdwe</li>
+        <li>dfytrkjiuer</li>
+        <li>erwqwedsapoil</li>
+        <li>rtewsdfukjnmc</li>
+        <li>erwuyyiwfg</li>
+        <li>trefdxcijknytr</li>
+        <li>uytrefd</li>
+        <li>tyrefdiu</li>
+        <li>uiytrd</li>
+        <li>rdfvtyiuiou</li>
+        <li>ytredfoiljkbn</li>
+        <li>esdtrfmnlkji</li>
+        <li>lkjpietfdsxcznvghty</li>
+        <li>lkjfghsadmnbbvccxz</li>
+        <li>adserfserfvc</li>
+        <li>luyhpowqe</li>
+        <li>qewrtyupoiua</li>
+        <li>wretrytuyu</li>
+        <li>awerytupqw</li>
+        <li>decssewrg</li>
+        <li>wertykjyksfe</li>
+        <li>wadsweqwefdr</li>
+        <li>ertyiefsfe</li>
+        <li>tredsxssddf</li>
+        <li>iuytreszc</li>
+        <li>ytrsdxryt</li>
+        <li>wsedrtyu</li>
+        <li>easedrftyui</li>
+        <li>tredsdrt</li>
+        <li>tresdkol</li>
+        <li>sertyusdvfuk</li>
+        <li>ertuyik</li>
+        <li>esdvxfty</li>
+        <li>wertyuiosdef</li>
+        <li>vfdrfetyui</li>
+        <li>asdfrgfiklo</li>
+        <li>loikmjhdsc</li>
+        <li>iolseryj</li>
+        <li>sdfggggggggggggggggggguy</li>
+        <li>sertyjyui</li>
+        <li>sfdrtkilio</li>
+        <li>sdfghyui</li>
+        <li>wretryuhiu</li>
+        <li>xdrtyuidfgyj</li>
+        <li>wefrfk,j,,jk</li>
+        <li>wdefrr</li>
+        <li>sfdfgykh</li>
+        <li>sdfgtyuiceyuj</li>
+        <li>sdfrrrrf</li>
+        <li>drtyuiyu</li>
+        <li>resdfxtyuijkl</li>
+        <li>yutrdfiuljk</li>
+        <li>iuytrsd</li>
+        <li>uytrdf</li>
+        <li>opiuytred</li>
+        <li>iuytrdxf</li>
+        <li>ersdxfcvujk</li>
+        <li>uytrdcx</li>
+        <li>uytres</li>
+        <li>iuytred</li>
+        <li>ytresd</li>
+        <li>tyresdfc</li>
+        <li>trewqasdczxvbnmnlkjpoiqewsadcxz</li>
+        <li>kjiutrqdsacxzvcnv</li>
+        <li>poiuytredfsbn</li>
+        <li>opiuytrdf</li>
+        <li>iuytrefdoiuy</li>
+        <li>poiuytre</li>
+        <li>iouytresd</li>
+        <li>opiuytrefdoiuytre</li>
+        <li>poiuytrewsd</li>
+        <li>poiewqsadcxz</li>
+        <li>poireqdsacxznbm</li>
+        <li>oiuytrdfuihj</li>
+        <li>iueoret</li>
+        <li>dsfaslakqwpo</li>
+        <li>jdopefvxc</li>
+        <li>nmdsqpo</li>
+        <li>mnvxckjhfdoi</li>
+        <li>sdjieu</li>
+        <li>kldjowiu</li>
+        <li>lkdsjfewury</li>
+        <li>dsjkhgiurtnv</li>
+        <li>jfkdgurit</li>
+        <li>kjyterwddfcv</li>
+        <li>iouytrskljegf</li>
+        <li>yuterdfssiojkv</li>
+        <li>uytreiouer</li>
+        <li>iouegfdcvxnmjy</li>
+        <li>qedsacxz</li>
+        <li>opiuytqsda</li>
+        <li>tyfdsiunb</li>
+        <li>iuytrfhjcb</li>
+        <li>poiuytqwsadlk</li>
+        <li>poerwds</li>
+        <li>poiuytreiuytr</li>
+        <li>oiuytrted</li>
+        <li>iurtdfsjkv</li>
+        <li>iouytresds</li>
+        <li>ytrefsdiou</li>
+        <li>oiuytredskh</li>
+        <li>iuytresfd</li>
+        <li>iuytrefdnbvc</li>
+        <li>iuytrdfkj</li>
+        <li>iuytresdkjhui</li>
+        <li>ytrdxuyoi</li>
+        <li>opewqadsvnb</li>
+        <li>oiuytrdfht</li>
+        <li>iuytfgxjn</li>
+        <li>fdsytriu</li>
+        <li>kjiuiurydf</li>
+        <li>iurewsftyuy</li>
+        <li>oiuytre</li>
+        <li>uytredfilkj</li>
+        <li>poiuywqsa</li>
+        <li>uytresdkjdx</li>
+        <li>iuytresyuj</li>
+        <li>iuytredsx</li>
+        <li>uytresdxuijkuyt</li>
+        <li>ytrediutr</li>
+        <li>lkhioyweq</li>
+        <li>iytreesd</li>
+        <li>erytytui</li>
+        <li>fdsuiyponbghyt</li>
+        <li>ytrdfxiuy</li>
+        <li>rtesuipo</li>
+        <li>uytredsuikjm</li>
+        <li>iuytredfxijk</li>
+        <li>opiuytredjguiyrtd</li>
+        <li>poiuytr</li>
+        <li>iouytresfdxiu</li>
+        <li>iuytrdf</li>
+        <li>esfdtiulkjlop</li>
+        <li>poiuytcxz</li>
+        <li>esryriu</li>
+        <li>koiuewrgf</li>
+        <li>iouytrefd</li>
+        <li>uytrdf</li>
+        <li>yutrdfc</li>
+        <li>iuytredf</li>
+        <li>ytredxcikj</li>
+        <li>uytrdfkj</li>
+        <li>uytrdijk</li>
+        <li>uytresdv</li>
+        <li>iuytre</li>
+        <li>yutr</li>
+        <li>tyrsdjkh</li>
+        <li>iuytresd</li>
+        <li>uytrdfiljk</li>
+        <li>uytred</li>
+        <li>uytrd</li>
+        <li>jdfoieu</li>
+        <li>dsfkjoefi</li>
+        <li>dkjfoifr</li>
+        <li>dkjfoief</li>
+        <li>jksdfor</li>
+        <li>jkdsioef</li>
+        <li>jdkkds</li>
+        <li>lkjfopr</li>
+        <li>dksjhfoeiw</li>
+        <li>ldskjoie</li>
+        <li>dskjheiow</li>
+        <li>skdjfeoiw</li>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
