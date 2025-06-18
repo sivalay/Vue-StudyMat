e4e5523206f474ef873a9b67d4a75372304f63e0
Siva <sivalayavinod25@gmail.com>
2025-06-18 22:47:46 +0530

    documentation change

--- src/components/VueDocNote.vue
+++ src/components/VueDocNote.vue
@@ -97,203 +97,25 @@
       </p>
     </div>
     <div>
-      <p>TypeScript</p>
+      <p>Git merge</p>
+      <p>Resolve the Conflict error.</p>
       <p>
-        <li>The ts promotes the Static Typing.</li>
-        <li>Error handling.</li>
-        <li>Early error detection handling.</li>
-        <li>Static Typing</li>
-        <li>jkytrfdui</li>
-        <li>uityrerfs</li>
-        <li>iuytrefd</li>
-        <li>jhkuyti</li>
-        <li>ytrefdrtuiut</li>
-        <li>uytrefdyi</li>
-        <li>ytresdcxertyui</li>
-        <li>ytrefdiu</li>
-        <li>uytrdfu</li>
-        <li>uytrewsduijkn</li>
-        <li>treyyiujk</li>
-        <li>esfrtttye</li>
-        <li>iuytrfkj</li>
-        <li>uytredf</li>
-        <li>uytrdsukj</li>
-        <li>ytredfuikj</li>
-        <li>uytredfik</li>
-        <li>ewqsdaxczvcfdrt</li>
-        <li>ioupokljmn</li>
-        <li>qewpoilkjdsacxzm</li>
-        <li>rdfriuy</li>
-        <li>redfyuiujkcv</li>
-        <li>lkjhfdsacxweqpoi</li>
-        <li>rtdgfuyncv</li>
-        <li>sfdxvijkntrdf</li>
-        <li>dfcvtyukj</li>
-        <li>wesdfcvertyupoq</li>
-        <li>rdfvikjdfvc</li>
-        <li>ewdsqazcxsdwe</li>
-        <li>dfytrkjiuer</li>
-        <li>erwqwedsapoil</li>
-        <li>rtewsdfukjnmc</li>
-        <li>erwuyyiwfg</li>
-        <li>trefdxcijknytr</li>
-        <li>uytrefd</li>
-        <li>tyrefdiu</li>
-        <li>uiytrd</li>
-        <li>rdfvtyiuiou</li>
-        <li>ytredfoiljkbn</li>
-        <li>esdtrfmnlkji</li>
-        <li>lkjpietfdsxcznvghty</li>
-        <li>lkjfghsadmnbbvccxz</li>
-        <li>adserfserfvc</li>
-        <li>luyhpowqe</li>
-        <li>qewrtyupoiua</li>
-        <li>wretrytuyu</li>
-        <li>awerytupqw</li>
-        <li>decssewrg</li>
-        <li>wertykjyksfe</li>
-        <li>wadsweqwefdr</li>
-        <li>ertyiefsfe</li>
-        <li>tredsxssddf</li>
-        <li>iuytreszc</li>
-        <li>ytrsdxryt</li>
-        <li>wsedrtyu</li>
-        <li>easedrftyui</li>
-        <li>tredsdrt</li>
-        <li>tresdkol</li>
-        <li>sertyusdvfuk</li>
-        <li>ertuyik</li>
-        <li>esdvxfty</li>
-        <li>wertyuiosdef</li>
-        <li>vfdrfetyui</li>
-        <li>asdfrgfiklo</li>
-        <li>loikmjhdsc</li>
-        <li>iolseryj</li>
-        <li>sdfggggggggggggggggggguy</li>
-        <li>sertyjyui</li>
-        <li>sfdrtkilio</li>
-        <li>sdfghyui</li>
-        <li>wretryuhiu</li>
-        <li>xdrtyuidfgyj</li>
-        <li>wefrfk,j,,jk</li>
-        <li>wdefrr</li>
-        <li>sfdfgykh</li>
-        <li>sdfgtyuiceyuj</li>
-        <li>sdfrrrrf</li>
-        <li>drtyuiyu</li>
-        <li>resdfxtyuijkl</li>
-        <li>yutrdfiuljk</li>
-        <li>iuytrsd</li>
-        <li>uytrdf</li>
-        <li>opiuytred</li>
-        <li>iuytrdxf</li>
-        <li>ersdxfcvujk</li>
-        <li>uytrdcx</li>
-        <li>uytres</li>
-        <li>iuytred</li>
-        <li>ytresd</li>
-        <li>tyresdfc</li>
-        <li>trewqasdczxvbnmnlkjpoiqewsadcxz</li>
-        <li>kjiutrqdsacxzvcnv</li>
-        <li>poiuytredfsbn</li>
-        <li>opiuytrdf</li>
-        <li>iuytrefdoiuy</li>
-        <li>poiuytre</li>
-        <li>iouytresd</li>
-        <li>opiuytrefdoiuytre</li>
-        <li>poiuytrewsd</li>
-        <li>poiewqsadcxz</li>
-        <li>poireqdsacxznbm</li>
-        <li>oiuytrdfuihj</li>
-        <li>iueoret</li>
-        <li>dsfaslakqwpo</li>
-        <li>jdopefvxc</li>
-        <li>nmdsqpo</li>
-        <li>mnvxckjhfdoi</li>
-        <li>sdjieu</li>
-        <li>kldjowiu</li>
-        <li>lkdsjfewury</li>
-        <li>dsjkhgiurtnv</li>
-        <li>jfkdgurit</li>
-        <li>kjyterwddfcv</li>
-        <li>iouytrskljegf</li>
-        <li>yuterdfssiojkv</li>
-        <li>uytreiouer</li>
-        <li>iouegfdcvxnmjy</li>
-        <li>qedsacxz</li>
-        <li>opiuytqsda</li>
-        <li>tyfdsiunb</li>
-        <li>iuytrfhjcb</li>
-        <li>poiuytqwsadlk</li>
-        <li>poerwds</li>
-        <li>poiuytreiuytr</li>
-        <li>oiuytrted</li>
-        <li>iurtdfsjkv</li>
-        <li>iouytresds</li>
-        <li>ytrefsdiou</li>
-        <li>oiuytredskh</li>
-        <li>iuytresfd</li>
-        <li>iuytrefdnbvc</li>
-        <li>iuytrdfkj</li>
-        <li>iuytresdkjhui</li>
-        <li>ytrdxuyoi</li>
-        <li>opewqadsvnb</li>
-        <li>oiuytrdfht</li>
-        <li>iuytfgxjn</li>
-        <li>fdsytriu</li>
-        <li>kjiuiurydf</li>
-        <li>iurewsftyuy</li>
-        <li>oiuytre</li>
-        <li>uytredfilkj</li>
-        <li>poiuywqsa</li>
-        <li>uytresdkjdx</li>
-        <li>iuytresyuj</li>
-        <li>iuytredsx</li>
-        <li>uytresdxuijkuyt</li>
-        <li>ytrediutr</li>
-        <li>lkhioyweq</li>
-        <li>iytreesd</li>
-        <li>erytytui</li>
-        <li>fdsuiyponbghyt</li>
-        <li>ytrdfxiuy</li>
-        <li>rtesuipo</li>
-        <li>uytredsuikjm</li>
-        <li>iuytredfxijk</li>
-        <li>opiuytredjguiyrtd</li>
-        <li>poiuytr</li>
-        <li>iouytresfdxiu</li>
-        <li>iuytrdf</li>
-        <li>esfdtiulkjlop</li>
-        <li>poiuytcxz</li>
-        <li>esryriu</li>
-        <li>koiuewrgf</li>
-        <li>iouytrefd</li>
-        <li>uytrdf</li>
-        <li>yutrdfc</li>
-        <li>iuytredf</li>
-        <li>ytredxcikj</li>
-        <li>uytrdfkj</li>
-        <li>uytrdijk</li>
-        <li>uytresdv</li>
-        <li>iuytre</li>
-        <li>yutr</li>
-        <li>tyrsdjkh</li>
-        <li>iuytresd</li>
-        <li>uytrdfiljk</li>
-        <li>uytred</li>
-        <li>uytrd</li>
-        <li>jdfoieu</li>
-        <li>dsfkjoefi</li>
-        <li>dkjfoifr</li>
-        <li>dkjfoief</li>
-        <li>jksdfor</li>
-        <li>jkdsioef</li>
-        <li>jdkkds</li>
-        <li>lkjfopr</li>
-        <li>dksjhfoeiw</li>
-        <li>ldskjoie</li>
-        <li>dskjheiow</li>
-        <li>skdjfeoiw</li>
+        <li>from the main branch</li>
+        <li>:git pull origin main</li>
+        <li>Go to the new branch</li>
+        <li>:git merge main</li>
+        <li>:git add .</li>
+        <li>:git commit -m ""</li>
+        <li>:git push origin branch/name</li>
+      </p>
+    </div>
+    <div>
+      <p>after merging</p>
+      <p>
+        <li>checkout to main branch.</li>
+        <li>:git checkout main</li>
+        <li>Then by git pull take all the changes to the local repo.</li>
+        <li>:git pull</li>
       </p>
     </div>
   </div>
--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -198,3 +198,39 @@ const handleSubmit = async () => {
   }
 }
 </script>
+
+<!-- <template>
+  <main>
+    -------------<employee-list />
+    <header-nav class="head" />
+    <router-view />
+  </main>
+</template>
+
+<script setup lang="ts">
+import HeaderNav from './components/HeaderNav.vue'
+// import EmployeeList from './components/EmployeeList.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  background-color: #7f5a83;
+  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
+  background-repeat: no-repeat;
+  color: #fff;
+  font-size: 18px;
+}
+.head {
+  text-align: center;
+  color: yellow;
+  padding: 40px 20px 30px;
+}
+</style> -->
+
+<!-- button {
+  padding: 10px;
+  margin-left: 10px;
+  cursor: pointer;
+} -->
